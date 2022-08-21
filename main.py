a9d47561de549f1626625ec2265bc6ce99b430d2
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-08-21 14:57:25 -0500

    cleaned up source data, and create charts

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import pandas as pd
 from pprint import pprint
+import matplotlib.pyplot as plt
+
 
 def load_file_to_db(filename, table, conn):
     # Read local csv file into pandas dataframe (dictionary object)
@@ -11,9 +13,19 @@ def load_file_to_db(filename, table, conn):
 
 
 def preview_data(table, conn):
+    # Print 10 rows of data from table
     sql = f"SELECT * FROM '{table}' limit 10;"
     dataframe = pd.read_sql_query(sql, con=conn)
     pprint(dataframe)
+    pprint(dataframe.columns)
+
+
+def create_chart(sql, conn, x, y):
+    # Fetch query results as pandas dataframe (dictionary object)
+    dataframe = pd.read_sql_query(sql, con=conn)
+    pprint(dataframe)
+    dataframe.plot(x=x, y=y, kind="bar")
+    plt.show()
 
 
 # Hard code table name and local file path
@@ -25,3 +37,15 @@ conn = sqlite3.connect("my_local.db")
 rows_updated = load_file_to_db(filename=source_file, table=table_name, conn=conn)
 # Preview the table
 preview_data(table=table_name, conn=conn)
+
+sql = f"""
+Select
+    AVG(Value) as avg_val,
+    Variable_category
+    from
+        {table_name}
+    GROUP BY Variable_category
+    ORDER BY AVG(Value)
+"""
+
+create_chart(sql=sql, conn=conn, x="Variable_category", y="avg_val")
