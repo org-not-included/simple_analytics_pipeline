18f4d85157a08f33a448748740a17315fd712deb
Siddarth Patel <siddarthpatel@Siddarths-MBP-2.lan1>
2022-10-04 18:47:21 -0400

    add employees csv

--- playground/queries.py
+++ playground/queries.py
@@ -2,6 +2,16 @@ import sqlite3
 import pandas as pd
 from my_playground import *
 
+def load_file_into_db(filename, table, conn):
+    """(dataframe.to_sql) load a table into a database connection istance
+
+    Args:
+        filename (string): name of the file
+        table (string): name of the table
+        conn (db): connection to sqlite database instance 
+    """
+    dataframe = pd.read_csv(filename)
+    dataframe.to_sql(name=table, con=conn, if_exists="replace")
 
 def preview_data_in_db(table, conn):
     """(pd.read_sql) read table contents in a database connection istance to print first 10 rows
@@ -15,15 +25,21 @@ def preview_data_in_db(table, conn):
     pprint(dataframe)
     
 def show_full_columns(table, conn):
+    """show columns datatypes (schema)
+
+    Args:
+        table (string): table name
+        conn (db): connection to sqlite database instance 
+    """
     sql = f"SELECT * FROM PRAGMA_TABLE_INFO('{table}');"
     dataframe = pd.read_sql(sql=sql, con=conn)
     pprint(dataframe)
     
 if __name__ == "__main__" :
-    source_file = "../data/sample_data.csv"
-    table_name = "sample_table"
-    conn = sqlite3.connect("my_local.db")
-    load_file_into_db(source_file, table_name, conn)
+    source_file = "../data/employees.csv"
+    table = "employees"
+    conn = sqlite3.connect("employees.db")
+    load_file_into_db(source_file, table, conn)
     preview_data_in_db(table_name, conn)
     show_full_columns(table_name, conn)
     
