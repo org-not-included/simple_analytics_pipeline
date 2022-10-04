7a5ec70ff7cc37f58b0a3501b3c5e20c700f6cae
Siddarth Patel <siddarthpatel@Siddarths-MBP-2.lan1>
2022-10-04 19:24:44 -0400

    add sql where query

--- playground/queries.py
+++ playground/queries.py
@@ -34,12 +34,24 @@ def show_full_columns(table, conn):
     sql = f"SELECT * FROM PRAGMA_TABLE_INFO('{table}');"
     dataframe = pd.read_sql(sql=sql, con=conn)
     pprint(dataframe)
-    
+
+def sql_where(table, conn):
+    print("First names of employees with salary greater than 5000")
+    sql = f"SELECT [first name] FROM '{table}' WHERE salary>5000;"
+    dataframe = pd.read_sql(sql=sql, con=conn)
+    pprint(dataframe)
+    print("First names, salary, gender of female employees with salary greater than 5000")
+    sql = f"SELECT [first name], salary, gender FROM '{table}' WHERE salary>5000 AND gender='Female' AND [first name] IS NOT Null;"
+    # sql2 = f"SELECT DISTINCT gender FROM {table} ORDER BY gender DESC;"
+    dataframe = pd.read_sql(sql=sql, con=conn)
+    pprint(dataframe)
+
 if __name__ == "__main__" :
     source_file = "../data/employees.csv"
     table = "employees"
     conn = sqlite3.connect("employees.db")
     load_file_into_db(source_file, table, conn)
-    preview_data_in_db(table_name, conn)
-    show_full_columns(table_name, conn)
+    preview_data_in_db(table, conn)
+    show_full_columns(table, conn)
+    sql_where(table, conn)
     
